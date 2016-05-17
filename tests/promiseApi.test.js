8765055fd985adda404f467436c94b4adc6626ef
Connor Weng <foamtea30@gmail.com>
2016-05-17 14:18:47 +0800

    introduce Crawler instance pool

--- tests/promiseApi.test.js
+++ tests/promiseApi.test.js
@@ -2,16 +2,48 @@
 
 var expect = require('chai').expect;
 var crawl = require('../lib/crawler').crawl;
+var instancePool = require('../lib/crawler').instancePool;
 var httpbinHost = 'localhost:8000';
 
 describe('crawl', function() {
+    beforeEach(function() {
+        instancePool.clear();
+    });
     it('should run the first readme example with the promise way', function(done) {
         crawl('http://' + httpbinHost, {
-            maxConnections: 10
+            maxConnections: 1
         }).then(function(response) {
             expect(response.statusCode).to.equal(200);
             expect(typeof response.body).to.equal('string');
             done();
         });
+    });
+    it('should use the same Crawler object if the options objects are same', function(done) {
+        crawl('http://' + httpbinHost, {
+            maxConnections: 2
+        }).then(function(response) {
+            expect(response.statusCode).to.equal(200);
+            return crawl('http://' + httpbinHost, {
+                maxConnections: 2
+            });
+        }).then(function(response) {
+            expect(response.statusCode).to.equal(200);
+            expect(instancePool.size()).to.equal(1);
+            done();
+        }).catch(done);
+    });
+    it('should use the different Crawler object if the options objects are different', function(done) {
+        crawl('http://' + httpbinHost, {
+            maxConnections: 3
+        }).then(function(response) {
+            expect(response.statusCode).to.equal(200);
+            return crawl('http://' + httpbinHost, {
+                maxConnections: 4
+            });
+        }).then(function(response) {
+            expect(response.statusCode).to.equal(200);
+            expect(instancePool.size()).to.equal(2);
+            done();
+        }).catch(done);
     });
 });
